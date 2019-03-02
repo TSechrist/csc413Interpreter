1000d970d3ff378a683926e263648237129547bb
Thomas Sechrist <tsechrist@mail.sfsu.edu>
2019-03-02 14:58:19 -0800

    Added bobcode

--- interpreter/bytecode/BopCode.java
+++ interpreter/bytecode/BopCode.java
@@ -6,13 +6,86 @@ import java.util.ArrayList;
 
 public class BopCode extends ByteCode {
 
+    private String operator;
+
     @Override
     public void init(ArrayList<String> argument) {
 
+        operator = argument.get(0);
     }
 
     @Override
     public void execute(VirtualMachine vm) {
 
+        int op1 = vm.popStack();
+        int op2 = vm.popStack();
+
+        if(operator.equals("+"))
+            vm.pushStack(op1 + op2);
+        else if(operator.equals("-"))
+            vm.pushStack(op2 - op1);
+        else if(operator.equals("-"))
+            vm.pushStack(op2 - op1);
+        else if(operator.equals("*"))
+            vm.pushStack(op2 * op1);
+        else if(operator.equals("/"))
+            vm.pushStack(op2 / op1);
+        else if(operator.equals("=="))
+        {
+            if(op2 == op1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals("!="))
+        {
+            if(op2 == op1)
+                vm.pushStack(0);
+            else
+                vm.pushStack(1);
+        }
+        else if(operator.equals("<"))
+        {
+            if(op2 < op1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals(">"))
+        {
+            if(op2 > op1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals("<="))
+        {
+            if(op2 <= op1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals(">="))
+        {
+            if(op2 >= op1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals("|"))
+        {
+            if(op2 == 1 || op1 == 1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
+        else if(operator.equals("&"))
+        {
+            if(op2 * op1 == 1)
+                vm.pushStack(1);
+            else
+                vm.pushStack(0);
+        }
     }
 }
+
--- interpreter/bytecode/ReadCode.java
+++ interpreter/bytecode/ReadCode.java
@@ -3,6 +3,7 @@ package interpreter.bytecode;
 import interpreter.VirtualMachine;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReadCode extends ByteCode {
 
@@ -15,6 +16,17 @@ public class ReadCode extends ByteCode {
 
     @Override
     public void execute(VirtualMachine vm) {
+
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("Enter a number");
+        input = stdin.nextLine();
+        try{
+            int value = Integer.parseInt(input);
+            vm.pushStack(value);
+        }
+        catch(Exception E){
+            System.out.println("Must input a number");
+        }
 
     }
 }
